7ec23ecf70fb6f815f4f61627f019383c57665ce
Matthew Sherborne <msherborne@gmail.com>
2021-05-17 20:55:48 +1000

    Can now send and receive 10 messages

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/joho/godotenv"
 )
 
-func pullMsgs(projectID string, sub *pubsub.Subscription) {
+func pullMsgs(projectID, subID string, done *sync.WaitGroup) {
+	defer done.Done()
 	ctx := context.Background()
 	client, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
@@ -18,12 +21,12 @@ func pullMsgs(projectID string, sub *pubsub.Subscription) {
 	}
 	defer client.Close()
 
-	log.Printf("Listening for messages: %s - %v", projectID, sub)
+	log.Printf("Listening for messages: %s - %s", projectID, subID)
 
 	// Consume 10 messages.
 	var mu sync.Mutex
 	received := 0
-	// sub := client.Subscription(subID)
+	sub := client.Subscription(subID)
 	cctx, cancel := context.WithCancel(ctx)
 	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
 		mu.Lock()
@@ -42,7 +45,7 @@ func pullMsgs(projectID string, sub *pubsub.Subscription) {
 
 /// Creates the pubub topic if it doesn't exist
 /// Panics on fail so no error is returned
-func ensureTopic(client *pubsub.Client, projectID, topicID string) *pubsub.Topic {
+func ensureTopic(client *pubsub.Client, projectID, topicID string) {
 	ctx := context.Background()
 
 	// See if our topic already exists
@@ -60,11 +63,9 @@ func ensureTopic(client *pubsub.Client, projectID, topicID string) *pubsub.Topic
 	} else {
 		log.Printf("Topic found: %v\n", topic)
 	}
-
-	return topic
 }
 
-func ensureSubscription(client *pubsub.Client, topic *pubsub.Topic, subID string) *pubsub.Subscription {
+func ensureSubscription(client *pubsub.Client, topicID, subID string) {
 	ctx := context.Background()
 
 	// List the subscriptions in case it's already there
@@ -72,7 +73,7 @@ func ensureSubscription(client *pubsub.Client, topic *pubsub.Topic, subID string
 	if sub == nil {
 		// Create the subscription
 		s, err := client.CreateSubscription(ctx, subID, pubsub.SubscriptionConfig{
-			Topic:       topic,
+			Topic:       client.Topic(topicID),
 			AckDeadline: 20 * time.Second,
 		})
 		if err != nil {
@@ -84,8 +85,48 @@ func ensureSubscription(client *pubsub.Client, topic *pubsub.Topic, subID string
 	} else {
 		log.Printf("Subscrition found: %v", sub)
 	}
+}
 
-	return sub
+func publishMsgs(projectID string, topicID string, n int, done *sync.WaitGroup) {
+	defer done.Done()
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("pubsub.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	var wg sync.WaitGroup
+	var totalErrors uint64
+	t := client.Topic(topicID)
+
+	for i := 0; i < n; i++ {
+		result := t.Publish(ctx, &pubsub.Message{
+			Data: []byte("Message " + strconv.Itoa(i)),
+		})
+
+		wg.Add(1)
+		go func(i int, res *pubsub.PublishResult) {
+			defer wg.Done()
+			// The Get method blocks until a server-generated ID or
+			// an error is returned for the published message.
+			id, err := res.Get(ctx)
+			if err != nil {
+				// Error handling code can be added here.
+				log.Printf("Failed to publish: %v", err)
+				atomic.AddUint64(&totalErrors, 1)
+				return
+			}
+			log.Printf("Published message %d; msg ID: %v\n", i, id)
+		}(i, result)
+	}
+
+	wg.Wait()
+
+	if totalErrors > 0 {
+		log.Fatalf("%d of %d messages did not publish successfully", totalErrors, n)
+	}
 }
 
 func main() {
@@ -103,8 +144,16 @@ func main() {
 	defer client.Close()
 
 	// Make the pubsub topic (if needed)
-	topic := ensureTopic(client, projectID, topicID)
-	subscription := ensureSubscription(client, topic, subID)
+	ensureTopic(client, projectID, topicID)
+	ensureSubscription(client, topicID, subID)
 
-	pullMsgs(projectID, subscription)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go pullMsgs(projectID, subID, &wg)
+	go publishMsgs(projectID, topicID, 10, &wg)
+
+	log.Printf("Waiting for all the messages")
+	wg.Wait()
+
 }
